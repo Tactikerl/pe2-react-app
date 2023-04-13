4ce92032b2232e6c64d6a6615c94f653ade5f4bb
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2023-04-13 18:33:06 +0200

    added base components for rendering venue listings

---
+++ src/components/VenueParams.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import DisplayVenues from "./DisplayVenue";
+
+const url = `https://nf-api.onrender.com/api/v1/holidaze/venues`;
+
+const VenueParams = () => {
+  const [fetchVenues, setFetchVenues] = useState([]);
+  const [venueSearch, setVenueSearch] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    async function requestFetchVenues() {
+      try {
+        setHasError(false);
+        setIsLoading(true);
+        const res = await fetch(url);
+        const contentType = res.headers.get(`Content-Type`);
+        if (contentType && contentType.includes("application/json")) {
+          const json = await res.json();
+          setFetchVenues(json);
+          setIsLoading(false);
+        } else {
+          setIsLoading(false);
+          setHasError(true);
+          throw new Error("Response is not in JSON format");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    requestFetchVenues();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading posts</div>;
+  }
+
+  if (hasError) {
+    return <div>Error Loading Data</div>;
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="venueSearch">
+          <input
+            type="text"
+            id="venueSearch"
+            value={venueSearch}
+            onChange={(e) => {
+              setVenueSearch(e.target.value);
+            }}
+            placeholder="Type to start search"
+          />
+        </label>
+        <button>Search</button>
+      </form>
+      <div className="venue-list">
+        {fetchVenues
+          .filter((venues) =>
+            venues.name.toLowerCase().includes(venueSearch.toLowerCase())
+          )
+          .map((venue) => (
+            <DisplayVenues
+              key={venue.id}
+              name={venue.name}
+              owner={venue.owner}
+              description={venue.description}
+              maxGuests={venue.maxGuests}
+              price={venue.price}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default VenueParams;
